c820ad5293c0fc6a210ca36796fa23f636d9851a
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-09-24 18:27:12 -0300

    Add js input validation

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const container = document.querySelector(".container");
-
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
@@ -18,28 +16,52 @@ function displayLibrary() {
   myLibrary.forEach((book, i) => {
     container.innerHTML += `
     <div class="book" id="${i}">
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <p>${book.pages} pages</p>
-      <p>Status: ${book.status}</p>
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <p>${book.pages} pages</p>
+    <p>Status: ${book.status}</p>
     </div>`;
   });
 }
 
+const container = document.querySelector(".container");
 const newBookBtn = document.querySelector("#newBookBtn");
 const dialog = document.querySelector("dialog");
 const closeDialogBtn = document.querySelector("#closeDialogBtn");
 const form = document.querySelector("#form");
 
-newBookBtn.addEventListener("click", () => dialog.setAttribute("open", true));
-closeDialogBtn.addEventListener("click", () => dialog.removeAttribute("open"));
+const openDialog = () => dialog.setAttribute("open", true);
+const closeDialog = () => {
+  dialog.removeAttribute("open");
+  form.reset();
+};
+
+const sanitizeInput = (input) =>
+  input
+    .trim()
+    .replace(/</g, "&lt;") // html tags
+    .replace(/>/g, "&gt;")
+    .replace(/\s{2,}/g, " "); // two or more spaces
+
+newBookBtn.addEventListener("click", openDialog);
+closeDialogBtn.addEventListener("click", closeDialog);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
+
+  const title = sanitizeInput(document.querySelector("#title").value);
+  const author = sanitizeInput(document.querySelector("#author").value);
   const pages = document.querySelector("#pages").value;
-  const status = document.querySelector("#status").value;
+  const status =
+    document.querySelector("#status").value === "0" ? "Read" : "Unread";
+
+  const onlyNumbers = /^\d+$/g;
+  if (!pages.match(onlyNumbers)) {
+    closeDialog();
+    return;
+  }
+
   const book = new Book(title, author, pages, status);
   addBookToLibrary(book);
   displayLibrary();
+  closeDialog();
 });
